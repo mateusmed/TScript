88a2c5ddc486570b44582fda060d3f72662c483b
Mateus Med <mateus.medeiros46@gmail.com>
2020-12-08 12:42:32 -0300

    about learning typescript

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-
-import * as express from 'express';
+import express from 'express';
+import routes from './routes'
 
 const app = express()
+app.use(routes)
 
-app.get('/', ((req, res) => {
-    return res.send('Hello World');
-}))
 
-app.listen(3333);
+app.listen(3000);
